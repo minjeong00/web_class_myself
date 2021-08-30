b79cb22092cded3575bf8ea3737c26bd1d5d96a1
minjeong00 <giguda9127@gmail.com>
2021-08-30 11:24:48 +0900

    8/30 web application class

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,23 +16,6 @@ from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-@login_required
-def hello_world(request):
-    if request.method == "POST":
-
-        a = request.POST.get('input_text')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = a
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
